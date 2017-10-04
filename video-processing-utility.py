60319c9592184c2ec1a89292b0b6df57b0a26591
G <igauravsehrawat@gmail.com>
2017-10-04 08:22:53 +0530

    Code refactored, workflow restated.

--- video-processing-utility.py
+++ video-processing-utility.py
@@ -1,7 +1,10 @@
 #!/usr/local/bin/python3
-
+import os
 import shlex
 import subprocess
+import json
+from prompt_toolkit.contrib.completers import WordCompleter
+from prompt_toolkit import prompt
 from time import sleep
 
 def while_true(what, process):
@@ -12,11 +15,11 @@ def while_true(what, process):
             print(what, "Ran successfully")
             return
         elif process.returncode < 0:
-            outs, errs = process.communicate()
+            outs, errs = process.stdout, process.stderr
             print(what, outs, errs)
             return
         elif process.returncode > 0:
-            outs, errs = process.communicate()
+            outs, errs = process.stdout, process.stderr
             print(outs, errs)
             print(what, "Have you installed all the tools?")
             return
@@ -29,26 +32,22 @@ def generate_banner():
     talk_year = input("Which year the talk was presented?: ")
 
     command = (
-        "sed -e 's/%title%/{0}/g' -e 's/%speaker%/"
-        "{1}/g' -e 's/%month%/{2}/g' -e 's/%year%/"
-        "{3}/g' ./title-template.svg").format(
+        """sed -e 's/%title%/{0}/g' -e 's/%speaker%/"""
+        """{1}/g' -e 's/%month%/{2}/g' -e 's/%year%/"""
+        """{3}/g' ./title-template.svg""").format(
             talk_title, talk_speaker, talk_month, talk_year)
     args = shlex.split(command)
-    print(args)
     with open("preface.svg", "w") as generated_content:
         process = subprocess.run(args, stdout=generated_content)
         while_true("SVG generation", process)
 
-def generate_png():
-    width = input("What is the recorded video frame's width:? ")
-    height = input("What is the  your recorded video frame's height:? ")
-
+def generate_png(width, height):
     command = (
         "convert preface.svg -density 150 -resize {0}x{1}! preface.png").format(
             width, height)
     args = shlex.split(command)
-    print(args)
-    process = subprocess.run(args)
+    process = subprocess.run(
+        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while_true("PNG Generation", process)
 
 def generate_preface_video():
@@ -58,7 +57,8 @@ def generate_preface_video():
         "0 preface-video.mp4"
     )
     args = shlex.split(command)
-    process = subprocess.run(args)
+    process = subprocess.run(
+        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while_true("Preface video generation", process)
 
 def trim_the_video():
@@ -83,7 +83,8 @@ def trim_the_video():
 
     process = subprocess
     args = shlex.split(command)
-    process = subprocess.run(args)
+    process = subprocess.run(
+        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while_true("Video trimming", process)
     return video_file_name
 
@@ -100,20 +101,30 @@ def video_info(video_file_name):
         video_info = json.loads(output)
         stream_info = video_info["streams"][0]
         wh = [stream_info["width"], stream_info["height"]]
-        print("wh: ", wh)
         return wh
     return None
 
+def make_uploadable_video(video_file_name):
+    print("Generating uploadable video")
+    command = (
+        """ffmpeg -i preface-video.mp4 -i trimmed-{0}.mp4 -filter_complex """
+        """ "[0:0] [0:1] [1:0] [1:1] concat=n=2:v=1:a=1 [v] [a]" -map "[v]" """
+        """-map "[a]"  uploadable{0}.mp4"""
+    ).format(video_file_name)
     process = subprocess
     args = shlex.split(command)
-    process = subprocess.run(args)
-    while_true("Video trimming", process)
+    process = subprocess.run(
+        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while_true("Making uploadable", process)
 
 def main():
+    # 1, 2, 3, 4, 5
     generate_banner()
-    generate_png()
+    video_file_name = trim_the_video()
+    width, height = video_info(video_file_name)
+    generate_png(width, height)
     generate_preface_video()
-    trim_the_video()
+    make_uploadable_video(video_file_name)
 
 if __name__ == "__main__":
     main()
